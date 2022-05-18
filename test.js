4312a2edbdb49b40f3d113875e0ccf2a40378792
vivek-78 <akepoguvivek14@gmail.com>
2022-05-18 22:33:48 +0530

    intial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+const name = 9
+console.log(typeof name);
+let arr = [1,2,3,4]
+console.log(arr);
+sampleObject = {
+    name:'vivek',
+    email:'vivek@gmail.com'
+}
+console.log(sampleObject.name);
+console.log(sampleObject.email)
